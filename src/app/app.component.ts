a67548147904a40b878edb444a680e97e24e0f30
Connie Leung <cleung334@gmail.com>
2024-03-03 14:51:44 +0800

    add: menu bar to support generate text from 1) text 2) text and image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { setTitle } from './utils/title.util';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   template: `
     <div>
+      <div class="menu-container">
+        <ul class="menu">
+          <li><a routerLink="/">Generate Text from Text Input</a></li>
+          <li>Generate Text from Text and Image Inputs</li>
+        </ul>
+      </div>
       <h2>{{ title }}</h2>
       <router-outlet />
     </div>
@@ -17,22 +23,29 @@ import { setTitle } from './utils/title.util';
       padding: 1rem;
     }
 
+    div.menu-container {
+      border: 1px solid back;
+      background: gold;
+    }
+
+    .menu {
+      display: flex;
+    }
+    .menu > li {
+      margin-right: 0.5rem;
+      list-style-type: none;
+    }
+
     h2 {
       margin-bottom: 0.5rem;
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Gemini AI Generate Text Demo';
 
   constructor() {
     setTitle();
   }
-
-  ngOnInit(): void {
-    // throw new Error('Method not implemented.');
-    // const generativeAI = import('@google/generative-ai');
-  }
-
 }
