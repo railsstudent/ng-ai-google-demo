c5f45f94e046c7af4a455d805b9eb0d782becc30
Connie Leung <cleung334@gmail.com>
2024-03-03 14:57:12 +0800

    feat: create AppMenuComponent

---
+++ src/app/app-menu/app-menu.component.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-app-menu',
+  standalone: true,
+  imports: [RouterLink],
+  template: `
+    <div class="menu-container">
+      <ul class="menu">
+        <li><a routerLink="/">Generate Text from Text Input</a></li>
+        <li>Generate Text from Text and Image Inputs (WIP)</li>
+      </ul>
+    </div>
+  `,
+  styles: `
+    div.menu-container {
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+      margin-bottom: 1rem;
+
+      border: 1px solid back;
+      background: gold;
+    }
+
+    .menu {
+      display: flex;
+    }
+    
+    .menu > li {
+      margin-right: 0.5rem;
+      list-style-type: none;
+    }  
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppMenuComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { AppMenuComponent } from './app-menu/app-menu.component';
 import { setTitle } from './utils/title.util';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, AppMenuComponent],
   template: `
     <div>
-      <div class="menu-container">
-        <ul class="menu">
-          <li><a routerLink="/">Generate Text from Text Input</a></li>
-          <li>Generate Text from Text and Image Inputs</li>
-        </ul>
-      </div>
-      <h2>{{ title }}</h2>
+      <app-app-menu />
+       <h2>{{ title }}</h2>
       <router-outlet />
     </div>
   `,
   styles: `
     div {
       padding: 1rem;
-    }
-
-    div.menu-container {
-      border: 1px solid back;
-      background: gold;
-    }
-
-    .menu {
-      display: flex;
-    }
-    .menu > li {
-      margin-right: 0.5rem;
-      list-style-type: none;
     }
 
     h2 {
