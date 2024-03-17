b0803adb18138d300b3dea6bcd285398f5db5caa
Connie Leung <cleung334@gmail.com>
2024-03-17 22:39:07 +0800

    chore: delete unused import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { Route } from '@angular/router';
 import { appConfig } from '~app/app.config';
 import { AppComponent } from './app/app.component';
 
